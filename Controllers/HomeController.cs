026ca71c96275f4ccd501aba4711d736c77845da
Aditya <adityaharish626@gmail.com>
2015-10-23 12:47:24 -0400

    Added About Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContosUniversity.DAL;
+using ContosUniversity.ViewModels;
 
 namespace ContosUniversity.Controllers
 {
     public class HomeController : Controller
     {
+        public SchoolContext db = new SchoolContext();
+
         public ActionResult Index()
         {
             return View();
@@ -15,9 +19,14 @@ namespace ContosUniversity.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            IQueryable<EnrollmentDateGroup> data = from student in db.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+             return View(data.ToList());
         }
 
         public ActionResult Contact()
@@ -25,6 +34,12 @@ namespace ContosUniversity.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
